3f319e2900e171a24a436556feb1e9e0e602b737
Ricky Bien <ricky.bien@kkday.com>
2019-10-29 16:35:39 +0800

    init an empty project

---
+++ tests/HelloTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class HelloTest
+ * @package Tests
+ */
+class HelloTest extends TestCase
+{
+    public function testGo()
+    {
+        $this->assertTrue(true);
+    }
+}
